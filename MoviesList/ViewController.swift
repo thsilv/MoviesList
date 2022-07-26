8637dea8352a1642ae5f87586b3c5025661c4a6c
thsilv <t.silvaemp@gmail.com>
2022-07-26 17:31:59 -0300

    Initial Commit

---
+++ MoviesList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesList
+//
+//  Created by Thiago Silva on 26/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
